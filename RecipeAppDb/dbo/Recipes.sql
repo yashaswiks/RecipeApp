c6ec43757c9dc18c3c1ece88ced1dae8d0663bef
yashaswi <yashaswi@nabkisan.org>
2023-10-02 22:53:40 +0530

    Update Recipes.sql
    Category and Owner Columns added to Recipes

--- RecipeAppDb/dbo/Recipes.sql
+++ RecipeAppDb/dbo/Recipes.sql
@@ -3,5 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Title] NVARCHAR(50) NULL, 
     [Instructions] NVARCHAR(500) NULL, 
-    [Category] NVARCHAR(50) NULL
+    [Category] NVARCHAR(50) NULL, 
+    [Owner] NVARCHAR(450) NULL, 
+    CONSTRAINT [FK_Recipes_ToAspNetUsers] FOREIGN KEY ([Owner]) REFERENCES [AspNetUsers]([Id])
 )

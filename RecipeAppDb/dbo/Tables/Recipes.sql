fccbd146916ff5bc18bdb38dc25290afe222e347
yashaswi <yashaswi@nabkisan.org>
2023-10-06 23:30:06 +0530

    Recipe, Category Tables improved

--- RecipeAppDb/dbo/Tables/Recipes.sql
+++ RecipeAppDb/dbo/Tables/Recipes.sql
@@ -3,7 +3,8 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Title] NVARCHAR(50) NOT NULL, 
     [Instructions] NVARCHAR(500) NOT NULL, 
-    [Category] NVARCHAR(50) NOT NULL, 
+    [CategoryId] INT NOT NULL, 
     [Owner] NVARCHAR(450) NOT NULL, 
-    CONSTRAINT [FK_Recipes_ToAspNetUsers] FOREIGN KEY ([Owner]) REFERENCES [AspNetUsers]([Id])
+    CONSTRAINT [FK_Recipes_ToAspNetUsers] FOREIGN KEY ([Owner]) REFERENCES [AspNetUsers]([Id]), 
+    CONSTRAINT [FK_Recipes_ToCategories] FOREIGN KEY ([CategoryId]) REFERENCES [Categories]([Id])
 )

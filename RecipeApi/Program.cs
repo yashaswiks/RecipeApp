c09cafd7356a001c9838abdabed960c36068aece
yashaswi <yashaswi@nabkisan.org>
2023-10-06 23:15:04 +0530

    DI Implemented

--- RecipeApi/Program.cs
+++ RecipeApi/Program.cs
@@ -1,25 +1,12 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using RecipeApi.Areas.Identity.Data;
-using RecipeApi.Data;
+using RecipeApi;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string 'Default' not found.");
-
-builder.Services.AddDbContext<RecipeApiContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services
-    .AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<RecipeApiContext>();
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.AddAuthServices();
+builder.AddStandardServices();
+builder.AddDbServices();
+builder.AddBusinessServices();
 
 var app = builder.Build();
 
@@ -31,8 +18,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseAuthentication(); ;
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ RecipeApi/RegisterServices.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RecipeApi.Areas.Identity.Data;
+using RecipeApi.Data;
+using RecipeApp.Business.Repository;
+using RecipeApp.Business.Repository.IRepository;
+using RecipeApp.Business.Services;
+using RecipeApp.Business.Services.IServices;
+using System.Text;
+
+namespace RecipeApi;
+
+public static class RegisterServices
+{
+    public static void AddAuthServices(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<RecipeApiContext>();
+
+        builder.Services.AddAuthorization(opts =>
+        {
+            opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+        });
+
+        builder.Services.AddAuthentication("Bearer")
+            .AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+                    ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.ASCII.GetBytes(
+                            builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+                };
+            });
+    }
+
+    public static void AddStandardServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+
+    public static void AddDbServices(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string 'Default' not found.");
+
+        builder.Services.AddDbContext<RecipeApiContext>(options =>
+            options.UseSqlServer(connectionString));
+        builder.Services.AddScoped<IDatabaseOptions, DatabaseOptions>();
+    }
+
+    public static void AddBusinessServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<ICategoriesRepository, CategoriesRepository>();
+    }
+}

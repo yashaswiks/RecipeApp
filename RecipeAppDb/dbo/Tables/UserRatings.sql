24e4c1771d6565e1e84405fd378261c0c7013f30
yashaswi <yashaswi@nabkisan.org>
2023-10-07 23:16:54 +0530

    UserRatings Table Created

---
+++ RecipeAppDb/dbo/Tables/UserRatings.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserRatings]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RecipeId] INT NOT NULL, 
+    [RatingMadeBy] NVARCHAR(450) NOT NULL, 
+    [RatingValue] INT NULL, 
+    CONSTRAINT [FK_UserRatings_ToRecipes] FOREIGN KEY ([RecipeId]) REFERENCES [Recipes]([Id]), 
+    CONSTRAINT [FK_UserRatings_ToAspNetUsers] FOREIGN KEY ([RatingMadeBy]) REFERENCES [AspNetUsers]([Id])
+)
